512be2fc685b32cabd088b93ecaf156fe1bf0568
Achyuth Mohan <achyuthmohan1@gmail.com>
2021-08-03 22:17:39 +0530

    created for loop for multiple destinations

--- travello/models.py
+++ travello/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Destination:
+    id:int
+    name:str
+    desc:str
+    price:int
+    
--- travello/views.py
+++ travello/views.py
@@ -1,6 +1,27 @@
+from travello.models import Destination
 from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Destination
 
 def index(request):
-     return render(request,"index.html")
+     dest1=Destination()
+     dest1.name="Mumbai"
+     dest1.img='services1.jpg'
+     dest1.desc="The city never sleeps"
+     dest1.price=7000
+
+     dest2=Destination()
+     dest2.name="Hyderabad"
+     dest2.img='services2.jpg'
+     dest2.desc="Awesome city"
+     dest2.price=8000
+
+     dest3=Destination()
+     dest3.name="Bangalore"
+     dest3.img='services3.jpg'
+     dest3.desc="Beautiful city"
+     dest3.price=9000
+     dests=[dest1,dest2,dest3]
+    
+     return render(request,"index.html",{'dests':dests})
