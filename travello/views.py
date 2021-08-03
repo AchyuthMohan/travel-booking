f5a2dbbdb21f85aa8273283116318c0558b69790
Achyuth Mohan <achyuthmohan1@gmail.com>
2021-08-03 17:17:58 +0530

    initial commit

---
+++ travello/views.py
@@ -0,0 +1,6 @@
+from django.http.response import HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+     return render(request,"index.html")
