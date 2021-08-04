7b3b8d574606c42d352cbf82109ecd15b07faac8
Achyuth Mohan <achyuthmohan1@gmail.com>
2021-08-04 17:28:45 +0530

    if condition added

--- travello/models.py
+++ travello/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
 class Destination:
     id:int
     name:str
     desc:str
     price:int
-    
+    offer:bool
--- travello/views.py
+++ travello/views.py
@@ -10,18 +10,21 @@ def index(request):
      dest1.img='services1.jpg'
      dest1.desc="The city never sleeps"
      dest1.price=7000
+     dest1.offer=False
 
      dest2=Destination()
      dest2.name="Hyderabad"
      dest2.img='services2.jpg'
      dest2.desc="Awesome city"
      dest2.price=8000
+     dest2.offer=True
 
      dest3=Destination()
      dest3.name="Bangalore"
      dest3.img='services3.jpg'
      dest3.desc="Beautiful city"
      dest3.price=9000
+     dest3.offer=True
      dests=[dest1,dest2,dest3]
     
      return render(request,"index.html",{'dests':dests})
