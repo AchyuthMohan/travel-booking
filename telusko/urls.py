59fc68a2a46b587955a7c38c34570fe4e2527134
Achyuth Mohan <achyuthmohan1@gmail.com>
2021-08-11 13:06:11 +0530

    created requirements.txt

--- telusko/urls.py
+++ telusko/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('controlpanel-356/', admin.site.urls),
     
     path('',include('travello.urls')),
     path('accounts/',include('accounts.urls'))
